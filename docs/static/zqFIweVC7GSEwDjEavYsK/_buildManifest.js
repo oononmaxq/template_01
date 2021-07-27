41951ab16aa56e48364e8b7a3537e9cd32e1c3ca
oononmaxq <oononmaxq@gmail.com>
2021-07-27 21:55:18 +0900

    build app

---
+++ docs/static/zqFIweVC7GSEwDjEavYsK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c6337e457336020fb286.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
